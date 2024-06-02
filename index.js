a03052b7240bc198b5cc8e0f485f39bb9f6cfa0c
piyushhhxyz <piyush.bhawsar1@gmail.com>
2024-06-02 18:11:13 +0530

    cors added

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const chatbotRoutes = require('./routes/chat');
+// const plannerRoutes = require('./routes/planner')
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api', chatbotRoutes);
+// app.use('/api', plannerRoutes);
 
 const port = process.env.PORT || 3000;
 app.get("/", (_, res) => res.send("SHUBH MANGAL PLANNINGs API Gateway Running"))

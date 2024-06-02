b405d3b98ad6f7e574bbe887bd52cac4b12a55a5
piyushhhxyz <piyush.bhawsar1@gmail.com>
2024-06-02 18:34:33 +0530

    AI planner added.

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 require('dotenv').config();
 
 const chatbotRoutes = require('./routes/chat');
-// const plannerRoutes = require('./routes/planner')
+const plannerRoutes = require('./routes/planner')
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/api', chatbotRoutes);
-// app.use('/api', plannerRoutes);
+app.use('/api', plannerRoutes);
 
 const port = process.env.PORT || 3000;
 app.get("/", (_, res) => res.send("SHUBH MANGAL PLANNINGs API Gateway Running"))
